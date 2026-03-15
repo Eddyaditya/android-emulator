4d1ec0a6aaeaa3c8128ed0a196f9691bbb21330f
Aditya Negi <152770252+Eddyaditya@users.noreply.github.com>
2026-03-15 14:55:16 +0530

    Update emulator.h to integrate QEMU and UI managers

---
+++ emulator.h
@@ -0,0 +1,28 @@
+#ifndef EMULATOR_H
+#define EMULATOR_H
+
+#include "qemu_manager.h"
+#include "ui_manager.h"
+#include <string>
+
+class Emulator {
+private:
+    QEMUManager qemuManager;
+    UIManager uiManager;
+    bool initialized;
+    bool running;
+    std::string version;
+
+public:
+    Emulator();
+    ~Emulator();
+    
+    bool initialize();
+    void start();
+    void run();
+    void shutdown();
+    bool isRunning() const;
+    std::string getVersion() const;
+};
+
+#endif // EMULATOR_H
